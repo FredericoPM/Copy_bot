e0402604020c191a35179b4b7eac43bb7b716ff1
Frederico Prado Marques <fredericopradomarques@gmail.com>
2021-11-15 19:41:40 -0300

    copying mensagens and images as embeds

--- discord_copy_bot/index.js
+++ discord_copy_bot/index.js
@@ -13,16 +13,54 @@ const client  = new Discord.Client({
         "GUILD_MESSAGE_REACTIONS"
     ],
 });
-require('dotenv').config()
-client.login(process.env['BOT_TOKEN'])
+
+function creat_embed(color, title, text, image, footer_name, footer_image){
+    return new MessageEmbed()
+	.setColor(color)
+	.setTitle(title)
+	.setDescription(text)
+	.setImage(image)
+	.setTimestamp()
+	.setFooter(footer_name, footer_image);
+}
 
 client.on("message", async message => {
-    if(message.channel.id == '909826408627462191'){
-        const channel = client.channels.cache.get('909901683826499644');
-        channel.send("teste");
+    if(message.channel.id == process.env['TEST_INPUT_ID']){
+        const channel = client.channels.cache.get(process.env['TEST_OUTPUT_ID']);
+        var img_url;
+        message.attachments.forEach((e)=>{
+            img_url = e.url;
+        })
+        var embed = creat_embed(
+            '#0099ff',
+            'Trade call',
+            message.content,
+            img_url, 
+            "Robozão", 
+            "https://uploads.metropoles.com/wp-content/uploads/2018/08/17184217/watchmen-2.jpg"
+        );
+        channel.send({ embeds: [embed] });
+    }else if(message.channel.id == process.env['INPUT_ID']){
+        const channel = client.channels.cache.get(process.env['OUTPUT_ID']);
+        var img_url;
+        message.attachments.forEach((e)=>{
+            img_url = e.url;
+        })
+        var embed = creat_embed(
+            '#0099ff',
+            'Trade call',
+            message.content,
+            img_url, 
+            "Robozão", 
+            "https://uploads.metropoles.com/wp-content/uploads/2018/08/17184217/watchmen-2.jpg"
+        );
+        channel.send({ embeds: [embed] });
     }
 })
 
 client.on('ready', () => {
     console.log("Connected")
 })
+
+require('dotenv').config()
+client.login(process.env['BOT_TOKEN'])
